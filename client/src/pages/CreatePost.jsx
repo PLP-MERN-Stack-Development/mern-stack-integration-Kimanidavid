ebd262b2c7d8335d0eb7cdf43ff38b8c97b11b5c
kimanidavid <kimanidavid341@gmail.com>
2025-11-15 08:48:43 +0300

    Final commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreatePost() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Create New Post</h1>
+      <p>Create post form will go here</p>
+    </div>
+  );
+}
+
+export default CreatePost;
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,10 +1,82 @@
 const Post = require('../models/Post');
+
 exports.createPost = async (req, res) => {
-  const post = await Post.create({ ...req.body, author: req.user.id });
-  res.status(201).json(post);
+  try {
+    const post = await Post.create({ ...req.body, author: req.user.id });
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
+
 exports.getPosts = async (req, res) => {
-  const posts = await Post.find().populate('author', 'name');
-  res.json(posts);
+  try {
+    const posts = await Post.find().populate('author', 'name');
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
-// Add getPost, updatePost, deletePost, addComment...
+
+exports.getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate('author', 'name');
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    if (post.author.toString() !== req.user.id) {
+      return res.status(403).json({ message: 'Not authorized' });
+    }
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    if (post.author.toString() !== req.user.id) {
+      return res.status(403).json({ message: 'Not authorized' });
+    }
+    await Post.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Post deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.addComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    const comment = {
+      text: req.body.text,
+      author: req.user.id,
+      createdAt: new Date()
+    };
+    post.comments.push(comment);
+    await post.save();
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
